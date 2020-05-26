4f91bd3fa68eaa80ec422016e55a3e6f2e7f38c7
Ellen Boeke <eb@avietech.com>
2020-05-26 07:13:09 -0600

    clean up

--- src/searchMovies.js
+++ src/searchMovies.js
@@ -1,12 +1,26 @@
-import React from "react";
+import React, {useState} from "react";
 
 export default function SearchMovies(){
     
+    // states: input query, movies
+    const [query, setQuery] = useState('');
+    
     const searchMovies = async (e) => {
         e.preventDefault();
-        console.log("submitting");
-        
-        //const url = `https://api.themoviedb.org/3/search/movie?api_key=5dcf7f28a88be0edc01bbbde06f024ab&language=en-US&query=${query}&page=1&include_adult=false`;
+
+        const query = "Jurassic Park";
+
+        const url = `https://api.themoviedb.org/3/search/movie?api_key=e7f3345bd27e08419ec7a9eb7f4f4f8f&language=en-US&query=${query}&page=1&include_adult=false`;
+
+        try {
+            const res = await fetch(url);
+            const data = await res.json();
+
+            console.log(data);
+        }
+        catch(error) {
+            console.log(error);
+        }
     }
     
     return (
